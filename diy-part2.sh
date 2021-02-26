5fa008c0a0708443e6673cce3f1dc9e39a7bf8f2
Miro11 <69784329+Miroxyz@users.noreply.github.com>
2021-02-26 08:22:26 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,21 +14,21 @@
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
 # 添加第三方软件包
-# svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-app-openclash package/luci-app-openclash
-svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-app-eqos package/luci-app-eqos
+svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-app-openclash package/kenzok8/luci-app-openclash
+svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-app-eqos package/kenzok8/luci-app-eqos
 # svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-app-adguardhome package/luci-app-adguardhome
-svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-theme-atmaterial package/luci-theme-atmaterial
-svn co https://github.com/destan19/OpenAppFilter/trunk package/OpenAppFilter
-svn co https://github.com/tty228/luci-app-serverchan/trunk package/luci-app-serverchan
+svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-theme-atmaterial package/kenzok8/luci-theme-atmaterial
+svn co https://github.com/destan19/OpenAppFilter/trunk package/destan19/OpenAppFilter
+svn co https://github.com/tty228/luci-app-serverchan/trunk package/tty228/luci-app-serverchan
 # svn co https://github.com/pymumu/luci-app-smartdns/branches/lede package/luci-app-smartdns
-svn co https://github.com/garypang13/luci-theme-edge/branches/18.06 package/luci-theme-edge
+svn co https://github.com/garypang13/luci-theme-edge/branches/18.06 package/garypang13/luci-theme-edge
 
 # 替换更新默认 argon 主题
 rm -rf package/lean/luci-theme-argon && svn co https://github.com/jerrykuku/luci-theme-argon/branches/18.06 package/jerrykuku/luci-theme-argon
 
 # passwall ssrplus+ vssr
-svn co https://github.com/xiaorouji/openwrt-passwall/trunk package/openwrt-passwall
-svn co https://github.com/fw876/helloworld/trunk package/lean/helloworld
+svn co https://github.com/xiaorouji/openwrt-passwall/trunk package/xiaorouji/openwrt-passwall
+svn co https://github.com/fw876/helloworld/trunk package/fw876/lean/helloworld
 svn co https://github.com/jerrykuku/lua-maxminddb/trunk package/jerrykuku/lua-maxminddb
 svn co https://github.com/jerrykuku/luci-app-vssr/trunk package/jerrykuku/luci-app-vssr
 
